a0193d08208a3f1b5bfbbee8602a610506ad864b
JamesEArnold <jearnold22@gmail.com>
2022-03-19 00:15:24 -0400

    :loud_sound: EW-001: implement winston logger

---
+++ server/src/utils/logger.ts
@@ -0,0 +1,24 @@
+import winston, { Logger, LoggerOptions } from 'winston';
+import config from '@/config';
+
+const loggerConfig: LoggerOptions = {
+  level: config.logLevel,
+  levels: winston.config.npm.levels,
+  format: winston.format.combine(
+    winston.format.timestamp({
+      format: 'YYYY-MM-DD HH:mm:ss',
+    }),
+    winston.format.errors({ stack: true }),
+    winston.format.splat(),
+    winston.format.json(),
+  ),
+  transports:
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.cli(),
+        winston.format.splat(),
+      ),
+    }),
+};
+
+export const logger: Logger = winston.createLogger(loggerConfig);
