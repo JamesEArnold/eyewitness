817bf166408bbe21e2a918f56e7541a3be41066b
JamesEArnold <jearnold22@gmail.com>
2022-03-21 17:05:35 -0400

    :sparkles: EW-002: implement list queues router

--- server/src/api/index.ts
+++ server/src/api/index.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
+import { awsCommandInput } from '@/services/aws-command-input';
 import healthCheck from '@/api/routes/health-check';
+import listQueues from '@/api/routes/list-queues';
 
 export default () => {
   const app: Router = Router();
   healthCheck(app);
+  listQueues(app, awsCommandInput);
   return app;
 };
