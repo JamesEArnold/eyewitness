18f6608fa343b98195bbdfee38ae1558d7fc833c
JamesEArnold <jearnold22@gmail.com>
2022-03-27 17:07:12 -0400

    :sparkles: EW-004: implement listing queues into vue front-end

---
+++ client/src/views/QueueList.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+import { onMounted, ref } from 'vue';
+import { Queue } from '@/domain/models/queue';
+import { queueRepositorySqs } from '@/infrastructure/instances/queueRepositorySqs';
+import { queueService } from '@/domain/services/queueService';
+
+const availableQueues = ref<Queue[]>();
+
+onMounted(async () => {
+  availableQueues.value = await queueService(queueRepositorySqs).listQueues();
+});
+</script>
+
+<template>
+  <div v-if="availableQueues">
+    {{ availableQueues }}
+  </div>
+</template>
+
+<style lang="scss">
+
+</style>
